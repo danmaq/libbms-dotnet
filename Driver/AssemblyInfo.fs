74a1a4e23bbcf1af0c24734bbfa794684fe22acb
Shuhei Nomura <info@danmaq.com>
2017-03-12 21:52:54 +0900

    Windows 版 Visual Studio で正しくビルド・実行できるようにした。

--- Driver/AssemblyInfo.fs
+++ Driver/AssemblyInfo.fs
@@ -1,19 +1,41 @@
-﻿namespace Driver
-open System.Reflection
+﻿namespace Driver.AssemblyInfo
 
-[<assembly: AssemblyTitle("Driver")>]
-[<assembly: AssemblyDescription("")>]
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("Driver for LibBMS")>]
+[<assembly: AssemblyDescription("LibBMS")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly: AssemblyProduct("Driver")>]
+[<assembly: AssemblyCopyright("Copyright(c) 2017 danmaq / DAYS PRODUCTION All rights reserved.")>]
 [<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
 
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+// ComVisible を false に設定すると、COM コンポーネントがこのアセンブリ内のその型を認識
+// できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[<assembly: ComVisible(false)>]
 
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります
+[<assembly: Guid("f177bcde-654c-4a7a-82ab-e4cd8d61e4fd")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
 
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
-
-()
+do
+    ()
--- Driver/Program.fs
+++ Driver/Program.fs
@@ -3,19 +3,20 @@ open System.IO
 open Danmaq.LibBMS
 
 //////////////////////////////////////////////////////////////////////
+/// <summary>引数からファイル名を抽出し、存在するなら返します。</summary>
 let exists argv =
     let exists f = Option.isSome f && Option.get f |> File.Exists
     let file = argv |> Array.tryItem 0
     if file |> exists then file else None
 
+/// <summary>ファイル名に対応する、ファイルの中身全行。</summary>
 let load file = file |> File.ReadAllLines
 
 ///<summary>テストも兼ねて適当なBMSを読んでパースするだけのツール</summary>
 [<EntryPoint>]
-let main argv = 
+let main argv =
     match argv |> exists with
         | None -> failwith "Usage: Driver.exe bmsfile.bms"
         | Some f -> f |> load |> Parser.parse |> printfn "%A"
     Console.ReadKey() |> ignore
     0 // return an integer exit code
-
